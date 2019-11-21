beff33eed0153b412883db60eeb0d199b047ec35
Joe Alexander <cambo7131@gmail.com>
2019-11-21 13:31:03 -0600

    Popover bootstrap code
    Added ngIf else logic to handle a quiz with no questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule
     , FormsModule
     , HttpClientModule
+    , NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
